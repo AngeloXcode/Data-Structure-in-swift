e62b31adff9519b357a0a638d727770a4bfa4b15
Angelo E Saber <Angelo E Saber>
2021-03-16 01:11:04 +0300

    Mafority of Element 2

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,25 @@
 import UIKit
+func majority(_ arr:[Int]) -> [Int]{
+    var dict   = [Int:Int]()
+     var result = [Int]()
+    
+    for item in arr {
+       // print("\(item)")
+        if  dict[item] != nil {
+            dict[item] = dict[item]! + 1
+        }else{
+            dict[item] = 1
+        }
+       
+    }
+    let k = 3
+    for(key,value) in dict{
+        if value > arr.count / k {
+            result.append(key)
+        }
+    }
+    return result
+}
 
-var str = "Hello, playground"
+
+print(majority([1,1,1,3,3,2,2,2]))
